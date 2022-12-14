b2a2f1f79ca244ae1942b7515354eaedbaed4918
Nurkhayat <hayatshripva@gmail.com>
2022-12-15 02:37:14 +0300

    1

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,10 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional(readOnly = true)
+   @Override
+   public User getSingelUserByCarModelAndCarSeries(String model, int series) {
+      return userDao.getSingelUserByCarModelAndCarSeries(model, series);
+   }
+
 }
